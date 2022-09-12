473179b652a7648eef90ea2368c4b39a822021d5
jose carballo <jczord@gmail.com>
2022-09-12 18:57:11 -0400

    iniciando conexion a base de datos

--- api/index.js
+++ api/index.js
@@ -5,13 +5,18 @@ const routers = require('./routers')
 
 // creamos el servidor
 const app = express();
+
 // habilitar el pasero de datos
 app.use(express.urlencoded({extended: true}));
+
 app.use(express.json());
 // habilitar cors para aceptar petidiones de otras url
+
 app.use(cors());
 // rutas del servidor
+
 app.use('/', routers());
+//puerto
 
 app.listen(4000, () => {
     console.log('servidor corriendo en el puerto: 4000');
